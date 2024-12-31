ba961608a955da108a5f7014db33cdd422a85b89
SaiSrichandra <saisrichandra@gmail.com>
2024-12-31 15:47:37 -0800

    Rm

--- railway-booking-system-backend/src/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const jwt = require('jsonwebtoken');
-const express = require('express');
-const router = express.Router();
-const loginContoller = require("../controllers/loginController");
-
-router.post("/login", loginContoller.login);
-
-module.exports = router;
